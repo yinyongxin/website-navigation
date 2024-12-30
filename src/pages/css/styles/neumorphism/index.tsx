9cb7051326d20f614102e2a39269d228f289be0c
yinyongxin0220 <yinyongxin0220@gmail.com>
2024-12-30 08:18:07 +0000

    feat: update

--- src/pages/css/styles/neumorphism/index.tsx
+++ src/pages/css/styles/neumorphism/index.tsx
@@ -10,16 +10,16 @@ export default () => {
       }}
       class="size-full grid grid-cols-1 grid-rows-2 xl:grid-cols-2 xl:grid-rows-1 p-10 place-items-center"
     >
-       <div class="w-full grid grid-cols-2 grid-rows-2 place-content-center place-items-center aspect-square">
-          <div
-            class="w-3/5 aspect-square "
-            style={{
-              "border-radius": rounded() + "px",
-              "box-shadow": `18px 18px 36px #bebebe,-18px -18px 36px #ffffff;`,
-            }}
-          ></div>
-        </div>
-      <div class="h-full flex flex-col justify-center "></div>
+      <div class="w-full grid grid-cols-2 grid-rows-2 place-content-center place-items-center aspect-square">
+        <div
+          class="w-3/5 aspect-square "
+          style={{
+            "border-radius": rounded() + "px",
+            "box-shadow": `18px 18px 36px #bebebe,-18px -18px 36px #ffffff;`,
+          }}
+        ></div>
+      </div>
+      <div class="w-full aspect-square">asdfasdf</div>
     </div>
   );
 };

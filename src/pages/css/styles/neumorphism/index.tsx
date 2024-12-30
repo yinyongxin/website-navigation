e6883a5fe29686c324f291525f6d76be500155ad
yinyongxin0220 <yinyongxin0220@gmail.com>
2024-12-30 08:45:28 +0000

    feat: update

--- src/pages/css/styles/neumorphism/index.tsx
+++ src/pages/css/styles/neumorphism/index.tsx
@@ -2,33 +2,46 @@ import { createSignal } from "solid-js";
 
 export default () => {
   const [color, setColor] = createSignal("#E0E0E0");
-  const [rounded, setRounded] = createSignal(20);
-  const [distalce, setDistalce] = createSignal(10);
-  const [blur, setBlur] = createSignal(30);
+  const [radius, setRadius] = createSignal(20);
+  const [distalce, setDistalce] = createSignal(8);
+  const [blur, setBlur] = createSignal(24);
   return (
     <div
       style={{
         "background-color": color(),
       }}
-      class="size-full grid grid-cols-1 grid-rows-2 xl:grid-cols-2 xl:grid-rows-1 p-10 place-items-center"
+      class="size-full grid grid-cols-1 grid-rows-2 xl:grid-cols-2 xl:grid-rows-1 p-10 place-items-center overflow-hidden"
     >
-      <div class="w-full grid grid-cols-2 grid-rows-2 place-content-center place-items-center aspect-square">
+      <div class="h-full xl:h-[unset] w-full xl:w-full grid grid-cols-2 grid-rows-2 place-content-center place-items-center aspect-square">
         <div
-          class="w-3/5 aspect-square "
+          class="h-3/5 xl:h-[unset] w-[unset] xl:w-3/5 aspect-square "
           style={{
-            "border-radius": rounded() + "px",
+            "border-radius": radius() + "px",
             "box-shadow": `${distalce()}px ${distalce()}px ${blur()}px #bebebe,-${distalce()}px -${distalce()}px ${blur()}px #ffffff;`,
           }}
         ></div>
         <div
-          class="w-3/5 aspect-square "
+          class="h-3/5 xl:h-[unset] w-[unset] xl:w-3/5 aspect-square "
           style={{
-            "border-radius": rounded() + "px",
+            "border-radius": radius() + "px",
             "box-shadow": `inset ${distalce()}px ${distalce()}px ${blur()}px #bebebe,inset -${distalce()}px -${distalce()}px ${blur()}px #ffffff;`,
           }}
         ></div>
       </div>
-      <div class="w-full aspect-square">asdfasdf</div>
+      <div class="h-full xl:h-[unset] w-full xl:w-full aspect-square flex flex-col gap-4">
+        <div class="flex justify-between">
+          <div>radius</div>
+          <div>{radius()}</div>
+        </div>
+        <div class="flex justify-between">
+          <div>distalce</div>
+          <div>{distalce()}</div>
+        </div>
+        <div class="flex justify-between">
+          <div>blur</div>
+          <div>{blur()}</div>
+        </div>
+      </div>
     </div>
   );
 };

c3d630b5f0b908a33e715a21c7933afa66bc4aaa
yinyongxin0220 <yinyongxin0220@gmail.com>
2024-12-31 03:00:01 +0000

    feat: update

--- src/pages/css/styles/neumorphism/index.tsx
+++ src/pages/css/styles/neumorphism/index.tsx
@@ -4,6 +4,12 @@ export default () => {
   const [radius, setRadius] = createSignal(20);
   const [distalce, setDistalce] = createSignal(8);
   const [blur, setBlur] = createSignal(24);
+  const getBaseNeumorphism = () => {
+    return {
+      "border-radius": radius() + "px",
+      "box-shadow": `${distalce()}px ${distalce()}px ${blur()}px #bebebe,-${distalce()}px -${distalce()}px ${blur()}px #ffffff;`,
+    };
+  };
   return (
     <div
       style={{
@@ -14,10 +20,7 @@ export default () => {
       <div class="h-full xl:h-[unset] w-full xl:w-full grid grid-cols-2 grid-rows-2 place-content-center place-items-center aspect-square">
         <div
           class="h-3/5 xl:h-[unset] w-[unset] xl:w-3/5 aspect-square "
-          style={{
-            "border-radius": radius() + "px",
-            "box-shadow": `${distalce()}px ${distalce()}px ${blur()}px #bebebe,-${distalce()}px -${distalce()}px ${blur()}px #ffffff;`,
-          }}
+          style={getBaseNeumorphism()}
         ></div>
         <div
           class="h-3/5 xl:h-[unset] w-[unset] xl:w-3/5 aspect-square "
@@ -27,16 +30,43 @@ export default () => {
           }}
         ></div>
       </div>
-      <div class="h-full xl:h-[unset] w-full xl:w-full aspect-square flex flex-col gap-4">
-        <div class="flex justify-between">
+      <div class="h-full xl:h-[unset] w-full xl:w-full aspect-square grid grid-cols-1 xl:grid-cols-2 gap-4 auto-rows-max grid-flow-dense content-center">
+        <div
+          class="flex justify-between px-6 py-6"
+          style={getBaseNeumorphism()}
+        >
           <div>radius</div>
-          <div>{radius()}</div>
+          <div class="flex items-center gap-3">
+            <div
+              class="cursor-pointer"
+              onClick={() => {
+                setRadius((r) => r - 1);
+              }}
+            >
+              -
+            </div>
+            {radius()}
+            <div
+              class="cursor-pointer"
+              onClick={() => {
+                setRadius((r) => r + 1);
+              }}
+            >
+              +
+            </div>
+          </div>
         </div>
-        <div class="flex justify-between">
+        <div
+          class="flex justify-between px-6 py-6"
+          style={getBaseNeumorphism()}
+        >
           <div>distalce</div>
           <div>{distalce()}</div>
         </div>
-        <div class="flex justify-between">
+        <div
+          class="flex justify-between px-6 py-6"
+          style={getBaseNeumorphism()}
+        >
           <div>blur</div>
           <div>{blur()}</div>
         </div>

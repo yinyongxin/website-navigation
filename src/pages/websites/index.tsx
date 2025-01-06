54ab7cd7e9c0e7d476552df1785ce94c4d507af5
yinyongxin0220 <yinyongxin0220@gmail.com>
2025-01-06 07:32:13 +0000

    feat: update

--- src/pages/websites/index.tsx
+++ src/pages/websites/index.tsx
@@ -14,7 +14,7 @@ const Content = () => {
   const list = [
     {
       title: "SolidJS",
-      url: "https://solidjs.com/",
+      url: "https://solidjs.com",
       description:
         "SolidJS 是一个轻量级、快速且可扩展的 JavaScript 库，用于构建用户界面。",
       icon: "data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20166%20155.3'%3e%3cpath%20d='M163%2035S110-4%2069%205l-3%201c-6%202-11%205-14%209l-2%203-15%2026%2026%205c11%207%2025%2010%2038%207l46%209%2018-30z'%20fill='%2376b3e1'/%3e%3clinearGradient%20id='a'%20gradientUnits='userSpaceOnUse'%20x1='27.5'%20y1='3'%20x2='152'%20y2='63.5'%3e%3cstop%20offset='.1'%20stop-color='%2376b3e1'/%3e%3cstop%20offset='.3'%20stop-color='%23dcf2fd'/%3e%3cstop%20offset='1'%20stop-color='%2376b3e1'/%3e%3c/linearGradient%3e%3cpath%20d='M163%2035S110-4%2069%205l-3%201c-6%202-11%205-14%209l-2%203-15%2026%2026%205c11%207%2025%2010%2038%207l46%209%2018-30z'%20opacity='.3'%20fill='url(%23a)'/%3e%3cpath%20d='M52%2035l-4%201c-17%205-22%2021-13%2035%2010%2013%2031%2020%2048%2015l62-21S92%2026%2052%2035z'%20fill='%23518ac8'/%3e%3clinearGradient%20id='b'%20gradientUnits='userSpaceOnUse'%20x1='95.8'%20y1='32.6'%20x2='74'%20y2='105.2'%3e%3cstop%20offset='0'%20stop-color='%2376b3e1'/%3e%3cstop%20offset='.5'%20stop-color='%234377bb'/%3e%3cstop%20offset='1'%20stop-color='%231f3b77'/%3e%3c/linearGradient%3e%3cpath%20d='M52%2035l-4%201c-17%205-22%2021-13%2035%2010%2013%2031%2020%2048%2015l62-21S92%2026%2052%2035z'%20opacity='.3'%20fill='url(%23b)'/%3e%3clinearGradient%20id='c'%20gradientUnits='userSpaceOnUse'%20x1='18.4'%20y1='64.2'%20x2='144.3'%20y2='149.8'%3e%3cstop%20offset='0'%20stop-color='%23315aa9'/%3e%3cstop%20offset='.5'%20stop-color='%23518ac8'/%3e%3cstop%20offset='1'%20stop-color='%23315aa9'/%3e%3c/linearGradient%3e%3cpath%20d='M134%2080a45%2045%200%2000-48-15L24%2085%204%20120l112%2019%2020-36c4-7%203-15-2-23z'%20fill='url(%23c)'/%3e%3clinearGradient%20id='d'%20gradientUnits='userSpaceOnUse'%20x1='75.2'%20y1='74.5'%20x2='24.4'%20y2='260.8'%3e%3cstop%20offset='0'%20stop-color='%234377bb'/%3e%3cstop%20offset='.5'%20stop-color='%231a336b'/%3e%3cstop%20offset='1'%20stop-color='%231a336b'/%3e%3c/linearGradient%3e%3cpath%20d='M114%20115a45%2045%200%2000-48-15L4%20120s53%2040%2094%2030l3-1c17-5%2023-21%2013-34z'%20fill='url(%23d)'/%3e%3c/svg%3e",
@@ -22,14 +22,14 @@ const Content = () => {
     },
     {
       title: "AstroJS",
-      url: "https://astro.build/",
+      url: "https://astro.build",
       description: "Astro 是一个用于构建快速、内容聚焦网站的 Web 框架。",
       icon: "https://astro.build/favicon.svg",
       tags: ["静态网站", "SolidJS", "ReactJS", "VueJS"],
     },
     {
       title: "Vite",
-      url: "https://vitejs.dev/",
+      url: "https://vitejs.dev",
       description:
         "Vite 是一个旨在为现代 Web 项目提供更快、更精简开发体验的构建工具。",
       icon: "https://vitejs.dev/logo.svg",
@@ -37,11 +37,11 @@ const Content = () => {
     },
     {
       title: "React",
-      url: "https://vitejs.dev/",
+      url: "https://react.dev",
       description:
         "Vite 是一个旨在为现代 Web 项目提供更快、更精简开发体验的构建工具。",
-      icon: "https://vitejs.dev/logo.svg",
-      tags: ["Build", "Vite"],
+      icon: "https://react.dev/favicon-32x32.png",
+      tags: ["React", "Vite"],
     },
     {
       title: "Nodejs中文网",
@@ -53,7 +53,7 @@ const Content = () => {
     },
     {
       title: "BestofJS",
-      url: "https://bestofjs.org/",
+      url: "https://bestofjs.org",
       description:
         "bestofjs.org 是一个社区驱动的网站，旨在帮助开发者发现和评估最优秀的 JavaScript 项目、库和工具。该网站通过收集和展示来自 GitHub 的数据，提供了一个全面的资源列表，帮助开发者找到适合他们需求的 JavaScript 项目。",
       icon: "https://bestofjs.org/favicon.ico",

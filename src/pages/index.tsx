373472cef7bd28c38ad96773456ec591bc9331fc
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-02-11 21:49:14 +0800

    refactorref(pagesactor(pages): 优化首页组件): 优化首页组件和格式
    - 调整和格式
    
    - 调整代码缩进和格式，提高可代码缩进和格式，提高可读性
    - 修复导航栏读性
    - 修复导航栏点击事件，确保正确设置活动状态点击事件，确保正确设置活动状态
    - 优化类型引用，使用
    - 优化类型引用，使用 ActiveEnum 而不是字符串字 ActiveEnum 而不是字符串字面量
    面量

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,63 +5,63 @@ import Theme from "../components/solid/home-block/theme";
 import AvatarBlock from "../components/solid/home-block/avatar-block";
 
 enum ActiveEnum {
-  All = "all",
-  Projects = "projects",
-  About = "about",
+	All = "all",
+	Projects = "projects",
+	About = "about",
 }
 
 const Content = () => {
-  const [active, setActive] = createSignal<ActiveEnum>(ActiveEnum.All);
-  const [list, setList] = createSignal([
-    <MyInfo />,
-    <Theme />,
-    <AvatarBlock />,
-  ]);
-  return (
-    <div class="bg-gray-100 py-8 sm:py-4 flex flex-col gap-8 items-center px-6">
-      <nav class="flex justify-center">
-        <ul class="bg-gray-200 flex gap-2 rounded-full p-2">
-          <li
-            class={cn("py-2 px-4 rounded-full cursor-pointer text-gray-500", {
-              "bg-white text-black": active() === "all",
-            })}
-            onClick={() => setActive("all")}
-          >
-            All
-          </li>
-          <li
-            class={cn("py-2 px-4 rounded-full cursor-pointer text-gray-500", {
-              "bg-white text-black": active() === "projects",
-            })}
-            onClick={() => setActive("projects")}
-          >
-            Projects
-          </li>
-          <li
-            class={cn("py-2 px-4 rounded-full cursor-pointer text-gray-500", {
-              "bg-white text-black": active() === "about",
-            })}
-            onClick={() => setActive("about")}
-          >
-            About
-          </li>
-        </ul>
-      </nav>
-      <div
-        class="grid w-full gap-6 justify-center"
-        style={{
-          "grid-template-columns": "repeat(auto-fill, 200px)",
-          "grid-template-rows": "repeat(10, 200px)",
-        }}
-      >
-        <For each={list()}>
-          {(item) => {
-            return item;
-          }}
-        </For>
-      </div>
-    </div>
-  );
+	const [active, setActive] = createSignal<ActiveEnum>(ActiveEnum.All);
+	const [list, setList] = createSignal([
+		<MyInfo />,
+		<Theme />,
+		<AvatarBlock />,
+	]);
+	return (
+		<div class="bg-gray-100 py-8 sm:py-4 flex flex-col gap-8 items-center px-6">
+			<nav class="flex justify-center">
+				<ul class="bg-gray-200 flex gap-2 rounded-full p-2">
+					<li
+						class={cn("py-2 px-4 rounded-full cursor-pointer text-gray-500", {
+							"bg-white text-black": active() === "all",
+						})}
+						onClick={() => setActive(ActiveEnum.All)}
+					>
+						All
+					</li>
+					<li
+						class={cn("py-2 px-4 rounded-full cursor-pointer text-gray-500", {
+							"bg-white text-black": active() === "projects",
+						})}
+						onClick={() => setActive(ActiveEnum.Projects)}
+					>
+						Projects
+					</li>
+					<li
+						class={cn("py-2 px-4 rounded-full cursor-pointer text-gray-500", {
+							"bg-white text-black": active() === "about",
+						})}
+						onClick={() => setActive(ActiveEnum.About)}
+					>
+						About
+					</li>
+				</ul>
+			</nav>
+			<div
+				class="grid w-full gap-6 justify-center"
+				style={{
+					"grid-template-columns": "repeat(auto-fill, 200px)",
+					"grid-template-rows": "repeat(10, 200px)",
+				}}
+			>
+				<For each={list()}>
+					{(item) => {
+						return item;
+					}}
+				</For>
+			</div>
+		</div>
+	);
 };
 
 export default Content;

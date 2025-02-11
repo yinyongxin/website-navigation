a2251ce30e23085ebb0552363cd81dd619d39d80
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-02-11 22:11:08 +0800

    refactor(components): 优化 AvatarBlock 组件代码
    - 删除多余的空行和导入语句
    - 统一代码格式，使用双引号替代单引号
    - 简化组件结构，提高代码可读性

--- src/components/solid/home-block/avatar-block.tsx
+++ src/components/solid/home-block/avatar-block.tsx
@@ -1,26 +1,25 @@
 import { Show, createSignal, onMount } from "solid-js";
 import { createAvatar } from "@dicebear/core";
 import { micah } from "@dicebear/collection";
-
 const AvatarBlock = () => {
-  const [avatar, setAvatar] = createSignal("");
-  onMount(() => {
-    const avatar = createAvatar(micah, {
-      seed: Math.random().toString(),
-      backgroundColor: ['ffd5dc', 'B6E3F4']
-      // ... other options
-    });
-    console.log('avatar', avatar.toJson())
-    const svg = avatar.toDataUri();
-    console.log("svg", svg);
-    setAvatar(svg);
-  });
-  return (
-    <div class="bg-white col-span-1 rounded-3xl hover:shadow-lg transition-shadow duration-400 grid justify-center overflow-hidden">
-      <Show when={avatar()}>
-        <img class="size-full" src={avatar()} alt="" />
-      </Show>
-    </div>
-  );
+	const [avatar, setAvatar] = createSignal("");
+	onMount(() => {
+		const avatar = createAvatar(micah, {
+			seed: Math.random().toString(),
+			backgroundColor: ["ffd5dc", "B6E3F4"],
+			// ... other options
+		});
+		console.log("avatar", avatar.toJson());
+		const svg = avatar.toDataUri();
+		console.log("svg", svg);
+		setAvatar(svg);
+	});
+	return (
+		<div class="bg-white col-span-1 rounded-3xl hover:shadow-lg transition-shadow duration-400 grid justify-center overflow-hidden">
+			<Show when={avatar()}>
+				<img class="size-full" src={avatar()} alt="" />
+			</Show>
+		</div>
+	);
 };
 export default AvatarBlock;

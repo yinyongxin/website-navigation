bb665a93509c50486ac740abd78fc97546c59826
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-02-11 22:09:03 +0800

    feat(components): 更新头像组件样式
    - 使用 micah 风格替换 lorelei 风格
    - 添加背景颜色和日志输出
    - 调整容器样式，添加 overflow-hidden 类

--- src/components/solid/home-block/avatar-block.tsx
+++ src/components/solid/home-block/avatar-block.tsx
@@ -1,20 +1,22 @@
 import { Show, createSignal, onMount } from "solid-js";
 import { createAvatar } from "@dicebear/core";
-import { lorelei } from "@dicebear/collection";
+import { micah } from "@dicebear/collection";
 
 const AvatarBlock = () => {
   const [avatar, setAvatar] = createSignal("");
   onMount(() => {
-    const avatar = createAvatar(lorelei, {
+    const avatar = createAvatar(micah, {
       seed: Math.random().toString(),
+      backgroundColor: ['ffd5dc', 'B6E3F4']
       // ... other options
     });
+    console.log('avatar', avatar.toJson())
     const svg = avatar.toDataUri();
     console.log("svg", svg);
     setAvatar(svg);
   });
   return (
-    <div class="p-6 bg-white col-span-1 rounded-3xl hover:shadow-lg transition-shadow duration-400 grid justify-center">
+    <div class="bg-white col-span-1 rounded-3xl hover:shadow-lg transition-shadow duration-400 grid justify-center overflow-hidden">
       <Show when={avatar()}>
         <img class="size-full" src={avatar()} alt="" />
       </Show>

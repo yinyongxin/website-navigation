d9357b362a9a146bd300b62188836a86932a95ea
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-02-12 01:00:06 +0800

    build(tailwind): 移除 daisyui 配置
    - 从 tailwind.config.js 中删除了 daisyui 插件的配置
    - 移除了 themes 数组，其中包括 "light"、"dark" 和 "cupcake" 主题

--- astro.config.js
+++ astro.config.js
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), solidJs()],
+	integrations: [solidJs()],
+	vite: {
+    plugins: [tailwindcss()],
+  },
 	// experimental: {
 	// 	svg: true,
 	// },

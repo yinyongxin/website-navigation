503d70c7b2effb31856c5ee3047b5bac25fae7e9
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-02-12 01:55:34 +0800

    style(solid): 优化主题切换按钮样式
    - 调整按钮背景、阴影和过渡效果，提升视觉体验
    - 优化选中状态下的样式变化，增加互动性

--- src/components/solid/home-block/theme.tsx
+++ src/components/solid/home-block/theme.tsx
@@ -30,11 +30,11 @@ const Theme = () => {
 				/>
 				<div
 					class={cn(
-						"relative w-full h-[50px] bg-base-300  rounded-full",
-						"after:content-[''] after:absolute after:rounded-full after:bg-amber-500 after:w-[40px] after:h-[40px] active:after:w-[50px]",
+						"relative w-full h-[50px] bg-base-300 rounded-full shadow-sm duration-300 after:duration-300 after:shadow-md",
+						"after:content-[''] after:absolute after:rounded-full after:bg-amber-500 peer-checked:after:bg-neutral",
+						" after:w-[40px] after:h-[40px] active:after:w-[50px]",
 						"after:top-[5px] after:left-[5px]",
-						"peer-checked:after:bg-neutral peer-checked:after:left-[105px] after:translate-x-[0%] peer-checked:after:translate-x-[-100%] ",
-						"shadow-sm duration-300 after:duration-300 after:shadow-md"
+						"peer-checked:after:left-[105px] peer-checked:after:translate-x-[-100%]"
 					)}
 				></div>
 				<svg

257547a0029337c33b2baa7cd4d8db720545fd8f
yinyongxin <1101278643@qq.com>
2025-02-14 07:26:27 +0000

    update

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -6,51 +6,51 @@ import TestBlock from "../components/solid/home-block/test-block";
 import SearchBlock from "../components/solid/home-block/search-block";
 import TetrisBlock from "../components/solid/home-block/tetris-block";
 export const TabsObj = {
-	all: {
-		title: "全部",
-	},
-	tools: {
-		title: "工具",
-	},
-	projects: {
-		title: "项目",
-	},
-	games: {
-		title: "游戏",
-	},
-	about: {
-		title: "关于我",
-	},
+  all: {
+    title: "全部",
+  },
+  tools: {
+    title: "工具",
+  },
+  projects: {
+    title: "项目",
+  },
+  games: {
+    title: "游戏",
+  },
+  about: {
+    title: "关于我",
+  },
 } as const;
 
 export type TabsKey = keyof typeof TabsObj;
 
 export const BlockList: {
-	key: TabsKey;
-	getContent: () => JSX.Element;
+  key: TabsKey;
+  getContent: () => JSX.Element;
 }[] = [
-	{
-		key: "about",
-		getContent: () => <MyInfo />,
-	},
-	{
-		key: "tools",
-		getContent: () => <AvatarBlock />,
-	},
-	{
-		key: "tools",
-		getContent: () => <Theme />,
-	},
-	{
-		key: "tools",
-		getContent: () => <SearchBlock />,
-	},
-	{
-		key: "projects",
-		getContent: () => <TestBlock />,
-	},
-	{
-		key: "games",
-		getContent: () => <TetrisBlock />,
-	},
+  {
+    key: "tools",
+    getContent: () => <AvatarBlock />,
+  },
+  {
+    key: "tools",
+    getContent: () => <Theme />,
+  },
+  {
+    key: "tools",
+    getContent: () => <SearchBlock />,
+  },
+  {
+    key: "projects",
+    getContent: () => <TestBlock />,
+  },
+  {
+    key: "games",
+    getContent: () => <TetrisBlock />,
+  },
+  {
+    key: "about",
+    getContent: () => <MyInfo />,
+  },
 ];

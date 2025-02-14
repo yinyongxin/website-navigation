a9f57a79777b4bb2fdd104d829050e7df4d3ca37
yinyongxin <1101278643@qq.com>
2025-02-14 03:20:37 +0000

    feat: update

--- src/components/solid/home-block/avatar-block.tsx
+++ src/components/solid/home-block/avatar-block.tsx
@@ -20,8 +20,8 @@ const AvatarBlock = () => {
       <Show when={avatar()} fallback={<div class="skeleton size-full"></div>}>
         <img class="size-full" src={avatar()} alt="" />
       </Show>
-      <div class="absolute bottom-0 left-0 right-0 backdrop-blur bg-base-100/30 drop-blur grid grid-cols-3 transition-transform duration-300 translate-y-full group-active:translate-y-0">
-        <div class="aspect-square flex justify-center items-center">
+      <div class="absolute bottom-0 left-0 right-0 border-t border-base-200/10 rounded-b-2xl overflow-hidden  bg-base-100/20 backdrop-blur-sm grid grid-cols-3 transition-[transform_opacity] duration-300 opacity-0 sm:group-hover:opacity-100 group-active:opacity-100 -translate-y-1/4 group-active:translate-y-0  sm:group-hover:translate-y-0">
+        <div class="aspect-square flex justify-center items-center rounded-bl-2xl">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -32,7 +32,7 @@ const AvatarBlock = () => {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-refresh-cw"
+            class="lucide lucide-refresh-cw group-hover:scale-110 group-active:scale-100  cursor-pointer"
           >
             <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
             <path d="M21 3v5h-5" />
@@ -40,7 +40,7 @@ const AvatarBlock = () => {
             <path d="M8 16H3v5" />
           </svg>
         </div>
-        <div class="aspect-square flex justify-center items-center border-x border-base-200/20">
+        <div class="aspect-square flex justify-center items-center border-x border-base-200/10">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -51,7 +51,7 @@ const AvatarBlock = () => {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-image-down"
+            class="lucide lucide-image-down group-hover:scale-110 group-active:scale-100 cursor-pointer"
           >
             <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21" />
             <path d="m14 19 3 3v-5.5" />
@@ -59,7 +59,7 @@ const AvatarBlock = () => {
             <circle cx="9" cy="9" r="2" />
           </svg>
         </div>
-        <div class="aspect-square flex justify-center items-center">
+        <div class="aspect-square flex justify-center items-center rounded-br-2xl ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -70,7 +70,7 @@ const AvatarBlock = () => {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-maximize"
+            class="lucide lucide-maximize group-hover:scale-110 group-active:scale-100 cursor-pointer"
           >
             <path d="M8 3H5a2 2 0 0 0-2 2v3" />
             <path d="M21 8V5a2 2 0 0 0-2-2h-3" />

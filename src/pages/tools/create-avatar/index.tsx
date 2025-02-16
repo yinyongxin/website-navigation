6f5784b43795ec653962860ac351d101b207a41a
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2025-02-16 22:54:23 +0800

    style(create-avatar): 优化竖省略号样式
    - 移除了 ellipsis-vertical 图标的高度和宽度属性
    - 添加了 w-full 类以使图标宽度充满容器

--- src/pages/tools/create-avatar/index.tsx
+++ src/pages/tools/create-avatar/index.tsx
@@ -210,15 +210,13 @@ export default () => {
 					<input type="checkbox" class="hidden" checked></input>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
 						viewBox="0 0 24 24"
 						fill="none"
 						stroke="currentColor"
 						stroke-width="2"
 						stroke-linecap="round"
 						stroke-linejoin="round"
-						class="lucide lucide-ellipsis-vertical"
+						class="lucide lucide-ellipsis-vertical w-full"
 					>
 						<circle cx="12" cy="12" r="1" />
 						<circle cx="12" cy="5" r="1" />

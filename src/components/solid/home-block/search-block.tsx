998f49b585268bdbb4a6959e0ba2d93cdaddbebd
yinyongxin <1101278643@qq.com>
2025-02-17 02:50:14 +0000

    update

--- src/components/solid/home-block/search-block.tsx
+++ src/components/solid/home-block/search-block.tsx
@@ -116,6 +116,30 @@ const SearchBlock = () => {
               );
             }}
           </For>
+          <div
+            class={cn(
+              "size-8 sm:size-10 flex justify-center items-center cursor-pointer"
+            )}
+          >
+            <a href="/tools/search">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-fullscreen size-4/5"
+              >
+                <path d="M3 7V5a2 2 0 0 1 2-2h2" />
+                <path d="M17 3h2a2 2 0 0 1 2 2v2" />
+                <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
+                <path d="M7 21H5a2 2 0 0 1-2-2v-2" />
+                <rect width="10" height="8" x="7" y="8" rx="1" />
+              </svg>
+            </a>
+          </div>
         </div>
         <div>
           <span class="text-primary font-bold text-xl ml-2">

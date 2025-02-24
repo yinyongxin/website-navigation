fb71613c008b49ed784e2ad445d76ee4f64afa4e
yinyongxin <1101278643@qq.com>
2025-02-24 08:06:34 +0000

    update

--- src/solid/components/calculator.tsx
+++ src/solid/components/calculator.tsx
@@ -1,18 +1,98 @@
 import { cn } from "../../utils";
+import { createSignal, For } from "solid-js";
+
 const Calculator = () => {
+  const [calculationHistory, setCalculationHistory] = createSignal<
+    {
+      expression: string;
+      result: string;
+    }[]
+  >([]);
+  const [currentInput, setCurrentInput] = createSignal([]);
+  const numberButtonClick = (value) => {
+    setCurrentInput([...currentInput(), value]);
+  };
+  function calculate(expression) {
+    // 替换 × 为 *，替换 ÷ 为 /
+    const newExpression = expression.map((item) => {
+      if (item === "multiply") {
+        return "*";
+      } else if (item === "/") {
+        return "/";
+      } else if (item === "subtract") {
+        return "-";
+      } else if (item === "add") {
+        return "+";
+      } else {
+        return item;
+      }
+    });
+    try {
+      // 使用 eval() 计算表达式
+      const result = eval(newExpression.join(""));
+      console.log(result);
+      return result;
+    } catch (error) {
+      return "无效的表达式";
+    }
+  }
+
+  const expressionShow = (expression) => {
+    const res = expression.map((item) => {
+      if (item === "multiply") {
+        return "*";
+      } else if (item === "/") {
+        return "/";
+      } else if (item === "subtract") {
+        return "-";
+      } else if (item === "add") {
+        return "+";
+      } else {
+        return item;
+      }
+    });
+    return res;
+  };
+
   return (
-    <div class="flex flex-col h-full">
-      <div class="grow">asds</div>
+    <div class="flex flex-col flex-1 gap-2">
+      <div class="grow flex flex-col justify-end items-end text-lg overflow-auto">
+        <div class="grow">
+          <For each={calculationHistory()}>
+            {(item) => {
+              return (
+                <div class="flex gap-1 items-center justify-end">
+                  <div>{expressionShow(item.expression)}</div>
+                  <div>=</div>
+                  <div>{item.result}</div>
+                </div>
+              );
+            }}
+          </For>
+        </div>
+        <div>{expressionShow(currentInput())}</div>
+      </div>
       <div
         class={cn(
           "flex-none grid gap-2 grid-cols-4 grid-rows-5",
           "*:active:scale-95 *:cursor-pointer *:aspect-square *:bg-base-300 *:rounded-lg *:flex *:justify-center *:items-center"
         )}
       >
-        <div class="bg-secondary-content! text-secondary">
+        <div
+          class="bg-secondary-content! text-secondary"
+          onClick={() => {
+            setCurrentInput([]);
+          }}
+        >
           <button class="w-full h-full">AC</button>
         </div>
-        <div class="bg-secondary-content! text-secondary">
+        <div
+          class="bg-secondary-content! text-secondary"
+          onClick={() => {
+            let newValue = currentInput().slice(0, -1);
+            setCurrentInput(newValue);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -46,7 +126,12 @@ const Calculator = () => {
             <circle cx="17.5" cy="17.5" r="2.5" />
           </svg>
         </div>
-        <div class="bg-secondary-content! text-secondary">
+        <div
+          class="bg-secondary-content! text-secondary"
+          onClick={() => {
+            setCurrentInput(currentInput() + "divide");
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -64,10 +149,36 @@ const Calculator = () => {
             <circle cx="12" cy="18" r="1" />
           </svg>
         </div>
-        <div class="">7</div>
-        <div class="">8</div>
-        <div class="">9</div>
-        <div class="bg-secondary-content! text-secondary">
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("7");
+          }}
+        >
+          7
+        </div>
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("8");
+          }}
+        >
+          8
+        </div>
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("9");
+          }}
+        >
+          9
+        </div>
+        <div
+          class="bg-secondary-content! text-secondary"
+          onClick={() => {
+            setCurrentInput([...currentInput(), "multiply"]);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -84,10 +195,36 @@ const Calculator = () => {
             <path d="m6 6 12 12" />
           </svg>
         </div>
-        <div class="">4</div>
-        <div class="">5</div>
-        <div class="">6</div>
-        <div class="bg-secondary-content! text-secondary">
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("4");
+          }}
+        >
+          4
+        </div>
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("5");
+          }}
+        >
+          5
+        </div>
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("6");
+          }}
+        >
+          6
+        </div>
+        <div
+          class="bg-secondary-content! text-secondary"
+          onClick={() => {
+            setCurrentInput([...currentInput(), "subtract"]);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -103,10 +240,36 @@ const Calculator = () => {
             <path d="M5 12h14" />
           </svg>
         </div>
-        <div class="">1</div>
-        <div class="">2</div>
-        <div class="">3</div>
-        <div class="bg-secondary-content! text-secondary">
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("1");
+          }}
+        >
+          1
+        </div>
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("2");
+          }}
+        >
+          2
+        </div>
+        <div
+          class=""
+          onClick={() => {
+            numberButtonClick("3");
+          }}
+        >
+          3
+        </div>
+        <div
+          class="bg-secondary-content! text-secondary"
+          onClick={() => {
+            setCurrentInput([...currentInput(), "add"]);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -123,7 +286,7 @@ const Calculator = () => {
             <path d="M12 5v14" />
           </svg>
         </div>
-        <div class="bg-primary-content! text-primary rounded-bl-2xl">
+        <div class="bg-primary-content! text-primary rounded-bl-2xl!">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -144,7 +307,16 @@ const Calculator = () => {
         </div>
         <div class="">0</div>
         <div class="">.</div>
-        <div class=" bg-primary-content! text-primary rounded-br-2xl">
+        <div
+          class=" bg-primary-content! text-primary rounded-br-2xl!"
+          onClick={() => {
+            setCalculationHistory([
+              ...calculationHistory(),
+              { expression: currentInput(), result: calculate(currentInput()) },
+            ]);
+            setCurrentInput([]);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
